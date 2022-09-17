32b49f001a72c0de074ace860b1cde3743fa54bd
Shechev <denisych777@gmail.com>
2022-09-17 17:50:37 +0300

    add Task 05 Максимальное из трёх

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -11,11 +11,3 @@ int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
 Console.WriteLine($"Квадрат числа {number} = {square}");
 
-1. Напишите программу, которая на вход принимает два
-числа и проверяет, является ли первое число квадратом
-второго.
-a = 25, b = 5 -> да
-a = 2, b = 10 -> нет
-a = 9, b = -3 -> да
-a = -3 b = 9 -> нет
-
---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("введите а:");
+int a = Convert.ToInt32(Console.ReadLine());// Запрашиваем I число
+Console.WriteLine("введите b:");
+int b = Convert.ToInt32(Console.ReadLine());// Запрашиваем II число
+Console.WriteLine("введите c:");
+int c = Convert.ToInt32(Console.ReadLine());// Запрашиваем III число
+
+int max = a; 
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"Максимальным числом является {max}");
