1d21d2d33d149822934f15fbc82e560cf187e5cc
Shechev <denisych777@gmail.com>
2022-09-17 20:42:32 +0300

    edit_01 Task 07 Чётные от 1 до N

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -3,13 +3,24 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+// Console.WriteLine("введите число:");
+// int N = Convert.ToInt32(Console.ReadLine());    // Запрашиваем число
+// int[] myArray = new int [] {0,0,0,0};           // Не разобрался как в таком случае выводить массив, поэтому он у меня на 4 ячейки... пока
+// int current = 2;
+// for (int count = 0; current <= N; count++)
+//     {
+//         myArray [count] = current;
+//         current+=2;
+//         Console.WriteLine(myArray [count]); 
+//     }
+
+
 Console.WriteLine("введите число:");
-int N = Convert.ToInt32(Console.ReadLine());// Запрашиваем число
-int[] myArray = new int [] {0,0,0,0};
+int N = Convert.ToInt32(Console.ReadLine());
 int current = 2;
-for (int count = 0; current <= N; count++)
-    {
-        myArray [count] = current;
-        current+=2;
-        Console.WriteLine(myArray [count]); 
-    }
+Console.Write($"Все чётные числа от 1 до N = {N}: ");
+while (current <= N)
+{
+    Console.Write(current + ", ");
+    current+=2;
+}
