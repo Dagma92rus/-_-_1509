9e112c060a0fb0dcf8f39822a37ea2f40886db45
Shechev <denisych777@gmail.com>
2022-09-27 12:13:48 +0300

    Lesson 3, Task 26, 28, 30

---
+++ Task24/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int SumNumbers2(int num)
+{
+    int sum = default;
+    int counter = 0;
+    while (counter <= num)
+    {
+        sum = sum + counter;
+        counter++;
+    }
+    return sum;
+}
+int result1 = SumNumbers(number);
+int result2 = SumNumbers2(number);
+Console.WriteLine($"{SumNumbers(number)} {result2}");
---
+++ Task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+System.Console.WriteLine("Загадайте число > ");
+int newNumber = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int QuantityDigit(int arg)
+{
+    int counter = default;
+    while (arg > 0)
+    {
+        arg = arg / 10;
+        counter++;
+    }
+    return counter;
+}
+Console.WriteLine($"Ваше число {newNumber} состоит из {QuantityDigit(newNumber)} цифр");
